fd271e8e0c91cb21e620ee977edc884437678599
Smita Narayan <snarayan@cimpress.com>
2020-06-11 19:38:26 -0400

    fixed link

--- game.js
+++ game.js
@@ -186,7 +186,7 @@ getNewQuestion = () => {
     const maxPossibleScore = POINTS * numQuestions;
     localStorage.setItem("score", score);
     localStorage.setItem("maxPossibleScore", maxPossibleScore);
-    window.location.href = "https://htmlpreview.github.io/?https://raw.githubusercontent.com/smitanarayan/quiz/master/game.html";
+    return window.location.assign("https://htmlpreview.github.io/?https://raw.githubusercontent.com/smitanarayan/quiz/master/end.html");
   }
 
   let questionIndex = questionCounter;
